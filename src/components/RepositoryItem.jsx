27532805b07108f0b8ffecec6b2e9c45d7834077
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2022-03-09 17:35:28 -0300

    added rocket repositories

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props)
         <li>
             <strong>{props.repository?.name ?? "Default"}</strong>
             <p>{props.repository.description}</p>
-            <a href={props.repository.link}>Acessar repo</a>
+            <a href={props.repository.html_url}>Acessar repo</a>
         </li>
     )
 }
